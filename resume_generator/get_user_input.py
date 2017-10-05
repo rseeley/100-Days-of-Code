8b4fe98281f74acf6bc110a8f84481b508c060d0
Ryan Seeley <ryseeley@gmail.com>
2017-10-04 20:01:17 -0500

    Add experience input; add recursive printing for testing input is saved correctly

--- resume_generator/get_user_input.py
+++ resume_generator/get_user_input.py
@@ -10,12 +10,57 @@ while True:
         years = input('Enter a date range for ' + school + ': ').strip()
         degree = input('Enter your degree from ' + school + ': ').strip()
         schools[school] = {
+            "School": school,
             "Years": years,
             "Degree": degree
         }
 
-for school in schools:
-    print(str.ljust("School:", 10), school)
+jobs = {}
+while True:
+    job = input(
+        'Enter your employment history, starting from the most recent ("q" to '
+        'quit): '
+    ).strip()
 
-    for detail in schools[school]:
-        print(str.ljust(detail + ':', 10), schools[school][detail])
+    if job == 'q':
+        break
+    else:
+        years = input('Enter a date range for ' + job + ': ').strip()
+
+        responsibilities = []
+        while True:
+            responsibility = input(
+                'Describe a responsibility you had during your employment at ' +
+                job + ' ("q" to quit): '
+            ).strip()
+
+            if responsibility == 'q':
+                break
+            else:
+                responsibilities.append(responsibility)
+
+        jobs[job] = {
+            "Job": job,
+            "Years": years,
+            "Responsibilities": responsibilities
+        }
+
+
+def mylistprint(l):
+    for i in l:
+        print('\t', i, '\n')
+
+
+def myprint(d):
+    for k, v in d.items():
+        if isinstance(v, list):
+            print(k + ': \n')
+            mylistprint(v)
+        elif isinstance(v, dict):
+            myprint(v)
+        else:
+            print(f"{k}: {v}")
+
+
+myprint(schools)
+myprint(jobs)
