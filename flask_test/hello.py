0121f32d886336665ca5eb486719995937bf87a0
Ryan Seeley <ryseeley@gmail.com>
2017-10-07 11:39:07 -0500

    Added to flask_test; create beginning of microblog Flask app

--- flask_test/hello.py
+++ flask_test/hello.py
@@ -1,37 +1,65 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, make_response, render_template, url_for
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-	return 'Index Page'
+    resp = make_response(render_template())
+    resp.set_cookie('username', 'the username')
+    username = request.cookies.get('username')
+    return 'Index Page'
+
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = make_response('page_not_found.html'), 404
+    resp.headers['X-Something'] = 'A value'
+    return resp
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
-	# show the user profile for that user
-	return f'User {username}'
+    # show the user profile for that user
+    return f'User {username}'
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-	# show the post with the given id, the id is an integer
-	return f'Post {post_id}'
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
 
 @app.route('/projects/')
 def projects():
-	return 'The project page'
+    return 'The project page'
+
 
 @app.route('/about')
 def about():
-	return 'The about page'
+    return 'The about page'
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-# 	if request.method == 'POST':
-# 		do_the_login()
-# 	else:
-# 		show_the_login_form()
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-	return render_template('hello.html', name=name)
-	
+    return render_template('hello.html', name=name)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
---
+++ microblog/config.py
@@ -0,0 +1,8 @@
+WTF_CSRF_ENABLED = True
+
+# TODO: change this
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://accounts.google.com/o/oauth2/v2/auth'}
+]
