45f2084a190d1f7696a262ace6e6c5df315b2e36
Ryan Seeley <ryseeley@gmail.com>
2017-10-14 17:12:28 -0500

    Add tests and templatetags for form fields

--- django-project/main/accounts/views.py
+++ django-project/main/accounts/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+
+from .forms import SignUpForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     context = {'form': form}
     return render(request, 'signup.html', context)
