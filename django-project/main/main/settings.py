2f303c33e7024207b0eae0637c2aff2c9e76c03a
Ryan Seeley <ryseeley@gmail.com>
2017-10-14 20:52:03 -0500

    Update tests; finish password change/reset

--- django-project/main/main/settings.py
+++ django-project/main/main/settings.py
@@ -129,5 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
