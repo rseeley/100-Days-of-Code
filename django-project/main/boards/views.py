b05dce9bf44b3aaeb9b0606c6ba7b2e510df7665
Ryan Seeley <ryseeley@gmail.com>
2017-10-16 21:40:24 -0500

    Refactor tests in 'boards'; add login_required decorator to new_topic view, comment out to avoid failing tests for now; start CBV to replace new_topic view

--- django-project/main/boards/views.py
+++ django-project/main/boards/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View
 
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
@@ -17,6 +19,7 @@ def board_topics(request, pk):
     return render(request, 'topics.html', context)
 
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in user
@@ -41,3 +44,31 @@ def new_topic(request, pk):
         form = NewTopicForm()
     context = {'board': board, 'form': form}
     return render(request, 'new_topic.html', context)
+
+
+# class NewTopicView(View):
+#     def __init__(self):
+#         self.board = get_object_or_404(Board, pk=self.pk)
+#         self.user = User.objects.first()
+
+#     def post(self, request):
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():
+#             topic = form.save(commit=False)
+#             topic.board = self.board
+#             topic.starter = self.user
+#             topic.save()
+
+#             post = Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=self.user
+#             )
+
+#             return redirect('board_topics', pk=self.board.pk)
+
+#     def get(self, request):
+#         form = NewTopicForm()
+
+#     context = {'board': board, 'form': form}
+#     return render(request, 'new_topic.html', context)
